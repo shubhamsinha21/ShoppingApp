58fb43238d8c315f2af192a921b12f0048677794
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-05-31 13:23:23 +0530

    shopping-app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//Navigation
+import { NavigationContainer } from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+import {View, Text,} from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
