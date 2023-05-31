9de0ead1f3eeadf5772218e98948c8f66e8a36d6
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-05-31 13:49:16 +0530

    home + details + index + App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React from 'react';
+import {View, Text} from 'react-native';
 
 //Navigation
-import { NavigationContainer } from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {View, Text,} from 'react-native'
+//screen
+import Home from './screens/Home';
+import Details from './screens/Details';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Details: {product: Product};
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Trending Products',
+          }}
+        />
+
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            title: 'Product Details',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
