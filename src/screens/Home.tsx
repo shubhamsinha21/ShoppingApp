fb523cf3df8638a63c579f7d45e572630d4c12a1
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-05-31 17:01:51 +0530

    home.tsx updated

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,47 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet, FlatList, Pressable} from 'react-native'
 
-const Home = () => {
+//React Navigation
+import {NativeStackScreenProps} from '@react-navigation/native-stack'
+import { RootStackParamList } from '../App'
+import ProductItem from '../components/ProductItem'
+import Separator from '../components/Separator'
+
+//data
+import { PRODUCTS_LIST } from '../data/constant'
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>
+
+const Home = ({navigation}:HomeProps) => {
   return (
-   <View>
-        <Text>
-                Home
-        </Text>
+   <View style={styles.container}>
+       <FlatList
+          data={PRODUCTS_LIST}
+          keyExtractor={item=> item.id}
+          ItemSeparatorComponent={Separator}
+          renderItem={({item}) => 
+          (
+              <Pressable onPress={() => {
+                navigation.navigate('Details', {
+                  product:item
+                })
+              }}>
+                <ProductItem product={item}/>
+              </Pressable>
+          )}
+       />
    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    padding:12,
+    backgroundColor:'#FFFFFF'
+  }
+})
 
 export default Home
